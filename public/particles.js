48e3b39fc3a64e9bf97b955cbee0cba72ff41ca7
Darshan Acharya <acharyadarshan02@gmail.com>
2022-08-16 14:07:17 +0545

    speed remove

--- public/particles.js
+++ public/particles.js
@@ -1,106 +1,105 @@
 class Particle {
-  /**
-   * @param  {Game} game
-   */
-  constructor(game) {
-    this.game = game;
-    this.checkForRemove = false;
-  }
-  update() {
-    this.x -= this.speedX + this.game.speed;
-    this.y -= this.speedY;
-    this.size *= 0.95;
-    if (this.size < 0.5) this.checkForRemove = true;
-  }
+    /**
+     * @param  {Game} game
+     */
+    constructor(game) {
+        this.game = game;
+        this.checkForRemove = false;
+    }
+    update() {
+        this.x -= this.speedX + this.game.speed;
+        this.y -= this.speedY;
+        this.size *= 0.95;
+        if (this.size < 0.5) this.checkForRemove = true;
+    }
 }
 
 export class Dust extends Particle {
-  /**
-   * @param  {Game} game
-   * @param  {number} x
-   * @param  {number} y
-   */
-  constructor(game, x, y) {
-    super(game);
-    this.size = Math.random() * 10 + 10;
-    this.x = x;
-    this.y = y;
-    this.speedX = Math.random();
-    this.speedY = Math.random();
-    this.color = "rgba(0,0,0,0.2)";
-  }
-  draw(context) {
-    context.beginPath();
-    context.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-    context.fillStyle = this.color;
-    context.fill();
-  }
+    /**
+     * @param  {Game} game
+     * @param  {number} x
+     * @param  {number} y
+     */
+    constructor(game, x, y) {
+        super(game);
+        this.size = Math.random() * 10 + 10;
+        this.x = x;
+        this.y = y;
+        this.speedX = Math.random();
+        this.speedY = Math.random();
+        this.color = "rgba(0,0,0,0.2)";
+    }
+    draw(context) {
+        context.beginPath();
+        context.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        context.fillStyle = this.color;
+        context.fill();
+    }
 }
 
 export class Splash extends Particle {
-  /**
-   * @param  {Game} game
-   * @param  {number} x
-   * @param  {number} y
-   */
-  constructor(game, x, y) {
-    super(game);
-    this.size = Math.random() * 100 + 100;
-    this.x = x - this.size * 0.4;
-    this.y = y - this.size * 0.4;
-    this.speedX = Math.random() * 6 - 3;
-    this.speedY = Math.random() * 2 + 2;
-    this.gravity = 0;
-    this.image = document.getElementById("fire");
-  }
-  update() {
-    super.update();
-    this.gravity += 0.1;
-    this.y += this.gravity;
-  }
-  draw(context) {
-    context.drawImage(this.image, this.x, this.y, this.size, this.size);
-  }
+    /**
+     * @param  {Game} game
+     * @param  {number} x
+     * @param  {number} y
+     */
+    constructor(game, x, y) {
+        super(game);
+        this.size = Math.random() * 100 + 100;
+        this.x = x - this.size * 0.4;
+        this.y = y - this.size * 0.4;
+        this.speedX = Math.random() * 6 - 3;
+        this.gravity = 0;
+        this.image = document.getElementById("fire");
+    }
+    update() {
+        super.update();
+        this.gravity += 0.1;
+        this.y += this.gravity;
+    }
+    draw(context) {
+        context.drawImage(this.image, this.x, this.y, this.size, this.size);
+    }
 }
 
 export class Fire extends Particle {
-  /**
-   * @param  {Game} game
-   * @param  {number} x
-   * @param  {number} y
-   */
-  constructor(game, x, y) {
-    super(game);
-    this.image = document.getElementById("fire");
-    this.size = Math.random() * 100 + 50;
-    this.x = x;
-    this.y = y;
-    this.speedX = 1;
-    this.speedY = 1;
-    this.angle = 0;
-    this.va = Math.random() * 0.2 - 0.1;
-  }
-  update() {
-    super.update();
-    this.angle += this.va;
-    this.x += Math.sin(this.angle * 5);
-  }
-  draw(context) {
-    //saves canvas state
-    context.save();
-    //moves canvas top left position to fire particles x and  y positions.
-    context.translate(this.x, this.y);
-    //rotates the canvas
-    context.rotate(this.angle);
+    /**
+     * @param  {Game} game
+     * @param  {number} x
+     * @param  {number} y
+     */
+    constructor(game, x, y) {
+        super(game);
+        this.image = document.getElementById("fire");
+        this.size = Math.random() * 100 + 50;
+        this.x = x;
+        this.y = y;
+        this.speedX = 1;
+        this.speedY = 1;
+        this.angle = 0;
+        this.va = Math.random() * 0.2 - 0.1;
+    }
+    update() {
+        super.update();
+        this.angle += this.va;
+        this.x += Math.sin(this.angle * 5);
+    }
+    draw(context) {
+        //saves canvas state
+        context.save();
+        //moves canvas top left position to fire particles x and  y positions.
+        context.translate(this.x, this.y);
+        //rotates the canvas
+        context.rotate(this.angle);
 
-    context.drawImage(
-      this.image,
-      -this.size * 0.5,
-      -this.size * 0.5,
-      this.size,
-      this.size
-    );
-    //restores the canvas state
-    context.restore();
-  }
+        context.drawImage(
+            this.image,
+            -this.size * 0.5,
+            -this.size * 0.5,
+            this.size,
+            this.size
+        );
+        //restores the canvas state
+        context.restore();
+    }
 }
