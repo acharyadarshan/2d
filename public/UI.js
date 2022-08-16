f0ad27dcd9ebec361162c2703dda89dcb2c0ef69
Darshan Acharya <acharyadarshan02@gmail.com>
2022-08-16 14:00:03 +0545

    font size change

--- public/UI.js
+++ public/UI.js
@@ -5,86 +5,86 @@ const GAMEOVER = new Audio();
 GAMEOVER.src = "assests/Sounds/gameOver.wav";
 
 export class UI {
-  /**
-   * @param  {Game} game
-   */
-  constructor(game) {
-    this.game = game;
-    this.fontSize = 30;
-    this.fontFamily = "Creepster";
-    this.livesImage = document.getElementById("lives");
-  }
-  draw(context) {
-    context.save();
-
-    context.font = this.fontSize + "px " + this.fontFamily;
-    context.textAlign = "left";
-    context.fillStyle = this.game.fontColor;
-    //score
-    context.fillText("SCORE: " + this.game.score, 20, 50);
-
-    //highscore
-    if (this.game.score > this.game.highScore) {
-      context.fillText("HIGH SCORE: " + this.game.score, 700, 50);
-    } else {
-      context.fillText("HIGH SCORE: " + this.game.highScore, 700, 50);
+    /**
+     * @param  {Game} game
+     */
+    constructor(game) {
+        this.game = game;
+        this.fontSize = 34;
+        this.fontFamily = "Creepster";
+        this.livesImage = document.getElementById("lives");
     }
+    draw(context) {
+        context.save();
 
-    //timer
-    context.font = this.fontSize * 0.8 + "px " + this.fontFamily;
-    context.fillText(
-      "Time: " + Math.abs((60 - this.game.time * 0.001).toFixed(1)),
-      20,
-      80
-    );
+        context.font = this.fontSize + "px " + this.fontFamily;
+        context.textAlign = "left";
+        context.fillStyle = this.game.fontColor;
+        //score
+        context.fillText("SCORE: " + this.game.score, 20, 50);
 
-    context.font = this.fontSize * 0.8 + "px " + this.fontFamily;
-
-    //ENERGY
-    context.fillText("Energy: ", 20, 110);
-    context.strokeRect(100, 90, 100, 20);
-    context.fillRect(100, 90, this.game.player.energy, 20);
-
-    //lives
-    context.fillText("Life: ", 20, 140);
-    context.strokeRect(100, 120, 100, 20);
-    context.fillRect(100, 120, this.game.lives, 20);
-
-    //target Score
-    context.fillText("Target: 60 ", 1300, 50);
-
-    //game over messages
-    if (this.game.gameOver) {
-      //Game win condition
-      if (this.game.score >= this.game.winningScore) {
-        this.game.gameOverDiv.classList.remove("hide");
-        this.game.gameStatusDiv.innerText = `YOU WIN!!!`;
-
+        //highscore
         if (this.game.score > this.game.highScore) {
-          this.game.gameHighScore.innerText = `HighScore: ${this.game.score}`;
+            context.fillText("HIGH SCORE: " + this.game.score, 700, 50);
         } else {
-          this.game.gameHighScore.innerText = `HighScore: ${this.game.highScore}`;
+            context.fillText("HIGH SCORE: " + this.game.highScore, 700, 50);
         }
 
-        this.game.gameScore.innerHTML = `Score: ${this.game.score} `;
-        this.game.sound.pause();
-        GAMEWIN.play();
+        //timer
+        context.font = this.fontSize * 0.8 + "px " + this.fontFamily;
+        context.fillText(
+            "Time: " + Math.abs((60 - this.game.time * 0.001).toFixed(1)),
+            20,
+            80
+        );
 
-        //Game lose condition
-      } else {
-        this.game.gameStatusDiv.innerText = `YOU LOSE!!!`;
-        this.game.gameScore.innerHTML = `Score: ${this.game.score}`;
+        context.font = this.fontSize * 0.8 + "px " + this.fontFamily;
 
-        if (this.game.score > this.game.highScore) {
-          this.game.gameHighScore.innerText = `HighScore: ${this.game.score}`;
-        } else {
-          this.game.gameHighScore.innerText = `HighScore: ${this.game.highScore}`;
+        //ENERGY
+        context.fillText("Energy: ", 20, 110);
+        context.strokeRect(100, 90, 100, 20);
+        context.fillRect(100, 90, this.game.player.energy, 20);
+
+        //lives
+        context.fillText("Life: ", 20, 140);
+        context.strokeRect(100, 120, 100, 20);
+        context.fillRect(100, 120, this.game.lives, 20);
+
+        //target Score
+        context.fillText("Target: 60 ", 1300, 50);
+
+        //game over messages
+        if (this.game.gameOver) {
+            //Game win condition
+            if (this.game.score >= this.game.winningScore) {
+                this.game.gameOverDiv.classList.remove("hide");
+                this.game.gameStatusDiv.innerText = `YOU WIN!!!`;
+
+                if (this.game.score > this.game.highScore) {
+                    this.game.gameHighScore.innerText = `HighScore: ${this.game.score}`;
+                } else {
+                    this.game.gameHighScore.innerText = `HighScore: ${this.game.highScore}`;
+                }
+
+                this.game.gameScore.innerHTML = `Score: ${this.game.score} `;
+                this.game.sound.pause();
+                GAMEWIN.play();
+
+                //Game lose condition
+            } else {
+                this.game.gameStatusDiv.innerText = `YOU LOSE!!!`;
+                this.game.gameScore.innerHTML = `Score: ${this.game.score}`;
+
+                if (this.game.score > this.game.highScore) {
+                    this.game.gameHighScore.innerText = `HighScore: ${this.game.score}`;
+                } else {
+                    this.game.gameHighScore.innerText = `HighScore: ${this.game.highScore}`;
+                }
+
+                this.game.gameOverDiv.classList.remove("hide");
+                this.game.sound.pause();
+                GAMEOVER.play();
+            }
         }
-
-        this.game.gameOverDiv.classList.remove("hide");
-        this.game.sound.pause();
-        GAMEOVER.play();
-      }
     }
-  }
 }
